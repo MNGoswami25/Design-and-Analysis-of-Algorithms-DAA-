0e8d28aedf3a3d09e20d759652035078e65a5f54
Manoj Nath Goswami <manoj2003nath@gmail.com>
2023-03-28 15:25:48 +0530

    first week

--- Week_1/Que1.c
+++ Week_1/Que1.c
@@ -1,3 +1,6 @@
+/*Given an array of nonnegative integers, design a linear algorithm and implement it using a
+program to find whether given key element is present in the array or not. Also, find total number
+of comparisons for each input case. (Time Complexity = O(n), where n is the size of input)*/
 #include<stdio.h>
 int linear_search(int* A,int k,int n){
     int i,count=0;
--- Week_1/Que3.c
+++ Week_1/Que3.c
@@ -1,3 +1,15 @@
+/*Given an already sorted array of positive integers, design an algorithm and implement it using a
+program to find whether a given key element is present in the sorted array or not. For an array
+arr[n], search at the indexes arr[0], arr[2], arr[4],.....,arr[2k
+
+] and so on. Once the interval (arr[2k
+] <
+
+key < arr[ 2k+1] ) is found, perform a linear search operation from the index 2k
+
+to find the element
+key. (Complexity < O(n), where n is the number of elements need to be scanned for searching):
+Jump Search*/
 #include<stdio.h>
 #include<math.h>
 int jumpSearch(int arr[], int k, int n,int* c){
